db176d9cfef0c0781b6e19b6942a64a28087bf65
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2023-12-11 17:59:26 +0100

    Completed 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simgitple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+function galleryMarkup(arr) {
+  const markup = arr
+    .map(
+      ({ original, preview, description }) => `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a></li>`
+    )
+    .join('');
+  galleryContainer.innerHTML = markup;
+}
+
+galleryMarkup(galleryItems);
+
+let lightBox = new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
