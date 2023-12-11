de6222b77e146f6670b6be22896939f2167bc20a
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2023-12-12 00:01:34 +0100

    Complete task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let currentTime = localStorage.getItem('videoplayer-current-time');
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
